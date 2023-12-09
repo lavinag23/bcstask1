c75d46b0e743888ea6e80be23e107d8391c94593
lavinag23 <153234155+lavinag23@users.noreply.github.com>
2023-12-09 13:53:12 +0530

    Add files via upload

---
+++ bcstask1sol.py
@@ -0,0 +1,54 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+bcs.py
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    bcs.py
+NameError: name 'bcs' is not defined
+
+======================================================== RESTART: C:\Users\lavin\AppData\Local\Programs\Python\Python312\bcs.py ========================================================
+                  Player  Mat  Inns  NO  Runs  ...   BF      SR  100  50  4s
+0            Jos Buttler   17    17   2   863  ...  579  149.05    4   4  83
+1              K L Rahul   15    15   3   616  ...  455  135.38    2   4  45
+2        Quinton De Kock   15    15   1   508  ...  341  148.97    1   3  47
+3          Hardik Pandya   15    15   4   487  ...  371  131.26    0   4  49
+4           Shubman Gill   16    16   2   483  ...  365  132.32    0   4  51
+..                   ...  ...   ...  ..   ...  ...  ...     ...  ...  ..  ..
+157     Fazalhaq Farooqi    3     1   1     2  ...    8   25.00    0   0   0
+158   Jagadeesha Suchith    5     2   0     2  ...    8   25.00    0   0   0
+159          Tim Southee    9     5   1     2  ...   12   16.66    0   0   0
+160  Nathan Coulter-Nile    1     1   1     1  ...    2   50.00    0   0   0
+161        Anrich Nortje    6     1   1     1  ...    6   16.66    0   0   0
+
+[162 rows x 12 columns]
+there are 0 null values
+     Runs  Inns  Mat
+0     863    17   17
+1     616    15   15
+2     508    15   15
+3     487    15   15
+4     483    16   16
+..    ...   ...  ...
+157     2     1    3
+158     2     2    5
+159     2     5    9
+160     1     1    1
+161     1     1    6
+
+[162 rows x 3 columns]
+     4s
+0    83
+1    45
+2    47
+3    49
+4    51
+..   ..
+157   0
+158   0
+159   0
+160   0
+161   0
+
+[162 rows x 1 columns]
+mse on train data 18.375617867007644
+mse on test data 12.828866404443731
